d7c4edf4e890ca57fc22ebb23ae464dc0db59fd7
joshuaa521 <joshuaa521@nycstudents.net>
2025-12-17 15:34:12 -0500

    Working on getLines method

--- src/Sign.java
+++ src/Sign.java
@@ -1,39 +1,45 @@
 public class Sign {
-    private String message
-    private int width
-    public Sign (String mess, int wth) {
+
+    private String message;
+    private int width;
+
+    public Sign(String mess, int wth) {
         message = mess;
         width = wth;
     }
-    public int numberOfLines(String mess, int wth) {
-        int length = mess.length();
+
+    public int numberOfLines() {
+        int length = message.length();
         int count;
         count = 0;
         int number = 0;
-        length / wth = number;
-        length % wth = count;
+        length / width = number;
+        length % width = count;
         if (count != 0) {
             number = count + number;
         }
         return number;
-    public String getLines (String mess, int wth) {
+    }
+
+    public String getLines(String mess, int wth) {
         int length = mess.length();
-        String final;
-        final = mess;
+        String final ;
+        final =mess;
         if (length == 0) {
-            final = null;
-            return final;
+            final =null;
+            return final ;
         }
         int divided = 0;
         if (length / number == 0) {
             divided = (length) / (number - 1)
-        }
-        else {
+        } else {
             divided = length / number;
         }
         String new;
-        new = mess.substring(0,divided) + ";" + mess.substring(0,divided * 2)
+        for (int i = 0; i <= divided; i++) {
+            new = mess.substring(0, i) + ";";
         }
-
     }
+
+}
 }
